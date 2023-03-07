107b02cb0e9454ee6cf1bbbf9984f4083c027330
Filipll97 <filiplindell2@gmail.com>
2023-03-07 15:23:19 +0100

    feat: Started Mapping, created profile maps and constructors containing instances of the automapper.

---
+++ AlumniNetworkAPI/Profiles/PostProfile.cs
@@ -0,0 +1,18 @@
+﻿using AlumniNetworkAPI.Dtos;
+using AlumniNetworkAPI.Models.Domain;
+using AlumniNetworkAPI.Models.Dtos.Posts;
+using AlumniNetworkAPI.Models.Dtos.Users;
+using AutoMapper;
+
+namespace AlumniNetworkAPI.Profiles
+{
+    public class PostProfile : Profile
+    {
+        public PostProfile()
+        {
+            CreateMap<PostCreateDto, Post>();
+            CreateMap<PostEditDto, Post>();
+            CreateMap<Post, PostReadDto>();
+        }
+    }
+}
---
+++ AlumniNetworkAPI/Profiles/UserProfile.cs
@@ -0,0 +1,17 @@
+﻿using AlumniNetworkAPI.Dtos;
+using AlumniNetworkAPI.Models.Domain;
+using AlumniNetworkAPI.Models.Dtos.Users;
+using AutoMapper;
+
+namespace AlumniNetworkAPI.Profiles
+{
+    public class UserProfile : Profile
+    {
+        public UserProfile()
+        {
+            CreateMap<UserCreateDto, User>();
+            CreateMap<UserEditDto, User>();
+            CreateMap<User, UserReadDto>();
+        }
+    }
+}
