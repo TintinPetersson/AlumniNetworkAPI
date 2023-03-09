6a07556d23cc4728428f7cd92b80cb696a6855f2
Tintin Petersson <tpsson95@gmail.com>
2023-03-09 15:44:28 +0100

    feat: login and logout function.

--- AlumniNetworkAPI/Models/Dtos/Users/UserCreateDto.cs
+++ AlumniNetworkAPI/Models/Dtos/Users/UserCreateDto.cs
@@ -2,10 +2,7 @@
 {
     public class UserCreateDto
     {
+        public string KeycloakId { get; set; }
         public string Username { get; set; }
-        public string Picture { get; set; }
-        public string Status { get; set; }
-        public string Bio { get; set; }
-        public string FunFact { get; set; }
     }
 }
