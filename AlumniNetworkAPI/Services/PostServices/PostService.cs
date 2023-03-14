17145a82ee21670508b871c55bab59b6150020e9
Filipll97 <filiplindell2@gmail.com>
2023-03-14 17:25:45 +0100

    feat: All endpoints for topic is completed

--- AlumniNetworkAPI/Services/PostServices/PostService.cs
+++ AlumniNetworkAPI/Services/PostServices/PostService.cs
@@ -1,13 +1,11 @@
 ﻿using AlumniNetworkAPI.Models.Domain;
 using Microsoft.EntityFrameworkCore;
 using System.Text.RegularExpressions;
-
 namespace AlumniNetworkAPI.Services.PostServices
 {
     public class PostService : IPostService
     {
         private readonly AlumniNetworkDbContext _context;
-
         public PostService(AlumniNetworkDbContext context)
         {
             _context = context;
@@ -15,7 +13,6 @@ namespace AlumniNetworkAPI.Services.PostServices
         public async Task<IEnumerable<Post>> GetPostsAsync(string keycloakId)
         {
             User user = _context.Users.First(u => u.KeycloakId == keycloakId);
-
             return await _context.Posts
                 .Include(c => c.Group)
                 .Include(c => c.Topic)
@@ -30,7 +27,6 @@ namespace AlumniNetworkAPI.Services.PostServices
         public async Task<IEnumerable<Post>> GetMessagesAsync(string keycloakId)
         {
             User user = _context.Users.First(u => u.KeycloakId == keycloakId);
-
             return await _context.Posts
                 .Where(c => c.RecieverId == user.Id)
                 .OrderByDescending(c => c.LastUpdated.Date)
@@ -40,15 +36,12 @@ namespace AlumniNetworkAPI.Services.PostServices
         public async Task<IEnumerable<Post>> GetPostByIdAsync(int id, string keycloakId)
         {
             User user = _context.Users.First(u => u.KeycloakId == keycloakId);
-
             return await _context.Posts
                 .Where(c => c.RecieverId == user.Id && c.AuthorId == id)
                 .OrderByDescending(c => c.LastUpdated.Date)
                 .ThenBy(c => c.LastUpdated.TimeOfDay)
                 .ToListAsync();
         }
-
-
         public async Task<IEnumerable<Post>> GetGroupPosts(int groupId)
         {
             return await _context.Posts
@@ -88,14 +81,12 @@ namespace AlumniNetworkAPI.Services.PostServices
                 .ThenBy(c => c.LastUpdated.TimeOfDay)
                 .ToListAsync();
         }
-
         public async Task UpdatePostAsync(Post post)
         {
             post.LastUpdated = DateTime.Now;
             _context.Entry(post).State = EntityState.Modified;
             await _context.SaveChangesAsync();
         }
-
         public async Task<Post> AddPostAsync(Post domainPost, string keycloakId)
         {
             User user = _context.Users
@@ -104,51 +95,34 @@ namespace AlumniNetworkAPI.Services.PostServices
             .Include(u => u.AcceptedEvents)
             .First(u => u.KeycloakId == keycloakId);
 
-            //if (domainPost.TopicId != null)
-            //{
-            //    Topic audienceToPostTo = _context.Topics.First(t => t.Id == domainPost.TopicId);
-            //    ICollection<Topic>? audienceRelation = user.Topics;
-            //    if (!audienceRelation.Contains(audienceToPostTo))
-            //    {
-            //        throw new Exception();
-            //    }
-            //}
-            //else if (domainPost.GroupId != null)
-            //{
-            //    Group audienceToPostTo = _context.Groups.First(g => g.Id == domainPost.GroupId);
-            //    ICollection<Group>? audienceRelation = user.Groups;
-            //    if (!audienceRelation.Contains(audienceToPostTo))
-            //    {
-            //        throw new Exception();
-            //    }
-            //}
-            //else if (domainPost.EventId != null)
-            //{
-            //    Event audienceToPostTo = _context.Events.First(e => e.Id == domainPost.EventId);
-            //    ICollection<Event>? audienceRelation = user.AcceptedEvents;
-            //    if (!audienceRelation.Contains(audienceToPostTo))
-            //    {
-            //        throw new Exception();
-            //    }
-            //}
-            //else if (domainPost.RecieverId != null || domainPost.ParentPostId != null)
-            //{
-            //    if (domainPost.RecieverId == user.Id)
-            //    {
-            //        throw new Exception();
-            //    }
-            //}
-            //else
-            //{
-            //    throw new KeyNotFoundException();
-            //}
+            try //[TODO]: Write better checks?, reciverId?
+            {
+                if (domainPost.TopicId != null)
+                {
+                    Topic audienceToPostTo = _context.Topics.First(t => t.Id == domainPost.TopicId);
+                    ICollection<Topic>? audienceRelation = user.Topics;
+                }
+                if (domainPost.GroupId != null)
+                {
+                    var audienceToPostTo = _context.Groups.First(g => g.Id == domainPost.GroupId);
+                    var audienceRelation = user.Groups; 
+                }
+                if (domainPost.EventId != null)
+                {
+                    Event audienceToPostTo = _context.Events.First(e => e.Id == domainPost.EventId);
+                    ICollection<Event>? audienceRelation = user.AcceptedEvents;
+                }
+            }
+            catch (Exception)
+            {
 
+                throw new KeyNotFoundException();
+            }
+          
             domainPost.AuthorId = user.Id;
             _context.Posts.Add(domainPost);
             await _context.SaveChangesAsync();
             return domainPost;
         }
-
-        
     }
 }
--- AlumniNetworkAPI/Services/TopicServices/ITopicService.cs
+++ AlumniNetworkAPI/Services/TopicServices/ITopicService.cs
@@ -4,7 +4,9 @@ namespace AlumniNetworkAPI.Services.TopicServices
 {
     public interface ITopicService
     {
-        Task<IEnumerable<Topic>> GetTopicsAsync(string keycloakId);
+        Task<IEnumerable<Topic>> GetTopicsAsync();
         Task<IEnumerable<Topic>> GetTopicsByIdAsync(int id);
+        Task<Topic> AddTopicAsync(Topic newTopic, string keycloakId);
+        Task AddTopicMembershipAsync(int topicId, string userId);
     }
 }
